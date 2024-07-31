384b323acf718741b5f434357eb8162eda4d9468
aashna-03 <ferroaash@gmail.com>
2024-07-31 22:23:18 +0530

    week2 task-3

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,7 +3,7 @@ const Button = () => {
 return (
 <>
 <button className="bg-pink-500 rounded text-white hover:scale-110
-px-5 py-2">
+px-5 py-2 mb-4 ">
 Button
 </button>
 </>
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+const footer = () => {
+  return (
+    <footer >
+    <div className="bg-white w-full mx-auto py-2 justify-evenly flex  ">
+        <div className="text-xl text-purple-900 underline decoration-pink-900"><Link href="/Add">Previous</Link></div>
+        <div className="text-xl text-pink-900 underline decoration-purple-900"><Link href="/Next">Next</Link></div>
+    </div>
+    </footer>
+  )
+}
+
+export default footer
