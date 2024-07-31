1fca9d2e8c2f82c99706721fa3e52a0639f8b246
aashna-03 <ferroaash@gmail.com>
2024-07-31 18:54:01 +0530

    week 2 task-2

---
+++ src/app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+    <div>
+    <h1 className="text-4xl text-center">404-Page Not Found</h1>
+    <p className="pt-4 text-center">The page you are looking for does not exist.</p>
+    </div>
+ )
+ }
