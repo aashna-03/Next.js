8a9c340eecebfe64a7487929f8c101d580da125c
aashna-03 <ferroaash@gmail.com>
2024-07-31 17:39:56 +0530

    week2 task1

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from '../Components/NavBar'
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NavBar/>
         {children}</body>
     </html>
   );
