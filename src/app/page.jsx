bf83a2cac3ed8dcad64af04076ca3fc463936b90
aashna-03 <ferroaash@gmail.com>
2024-08-01 10:31:23 +0530

    week2 task-4

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,17 +2,30 @@ import React from 'react'
 import HeaderPage from './Header/HeaderPage'
 import Button from "@components/Button"
 import Footer from'../Components/Footer'
+import picture from "/public/image2.jpg"
+import Image from 'next/image'
+
+
+
 
 
 const page = () => {
   return (
     <>
 
-     <div style={{ backgroundColor: 'black', minHeight: '30vh' }}></div>
+     <div style={{ backgroundColor: 'black', minHeight: '10vh' }}></div>
+     <Image
+        src={picture}
+        alt="code"
+        width={100}
+        height={100}
+      className="rounded-full w-56 h-56 ml-auto px-2 mr-20  "
+      /> 
 
     <HeaderPage/>
     <Button/>
     <Footer/>
+   
  
 
     
