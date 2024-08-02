7f290a6625b17334adc7f5e46dd80718fd368a75
aashna-03 <ferroaash@gmail.com>
2024-08-02 15:48:18 +0530

    added icon

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import { RiStarSmileLine } from "react-icons/ri";
 
 const footer = () => {
   return (
     <footer >
+      
     <div className="bg-white w-full mx-auto py-2 justify-evenly flex  ">
+    <RiStarSmileLine className="text-purple-900 hover:text-pink-900 mr-4" size={35}  />
         <div className="text-xl text-purple-900 underline decoration-pink-900"><Link href="/Add">Previous</Link></div>
         <div className="text-xl text-pink-900 underline decoration-purple-900"><Link href="/Next">Next</Link></div>
     </div>
