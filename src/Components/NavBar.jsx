2ea5d0ab1e5a8c59f13e19b7335df6b24883f87e
aashna-03 <ferroaash@gmail.com>
2024-08-03 13:06:09 +0530

    changed icon

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
-import { FaBomb } from 'react-icons/fa';
+import { PiPottedPlantBold } from "react-icons/pi";
 
 function Navbar() {
   return (
     <nav>
       <div className="bg-white w-full mx-auto py-4 justify-evenly flex">
-        <FaBomb className="text-purple-900 hover:text-pink-900 mr-4" size={35} />
+      <PiPottedPlantBold  className="text-purple-900 hover:text-pink-900 mr-4" size={35} />
         <div>
           <Link href="/Home">
             <button className="bg-transparent hover:bg-pink-800 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-900 hover:border-transparent rounded">
