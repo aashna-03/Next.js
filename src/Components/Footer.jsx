fe98d27981aa2c6e3c4e1d12ff5a06829cb68375
aashna-03 <ferroaash@gmail.com>
2024-08-04 12:39:18 +0530

    ecommerce website linked to week 1 final task page

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,7 +9,7 @@ const footer = () => {
     <div className="bg-white w-full mx-auto py-2 justify-evenly flex  ">
     <RiStarSmileLine className="text-purple-900 hover:text-pink-900 mr-4" size={35}  />
         <div className="text-xl text-purple-900 underline decoration-pink-900"><Link href="/Add">Previous</Link></div>
-        <div className="text-xl text-pink-900 underline decoration-purple-900"><Link href="/Next">Next</Link></div>
+        <div className="text-xl text-pink-900 underline decoration-purple-900"><Link href="/HomePage">Next(e-commerce)</Link></div>
     </div>
     </footer>
   )
